e5fce884e8f594c904e2413c67dd0d8ea73db687
Rafael-Ubuntu <leafar.huaman@gmail.com>
2013-10-13 15:47:34 -0400

    Add first test around todo title

--- src/tests.rb
+++ src/tests.rb
@@ -1,3 +1,12 @@
 require_relative './todo_list'
 
-# do stuff here
+# when i create a Todo , the title gets assigned
+todo1 = Todo.new("get milk")
+
+if todo1.title == "get milk"
+	puts "title was assigned"
+else
+	puts "title wasn't assigned - #{todo1.title}"
+end
+
+
